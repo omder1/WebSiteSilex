b9c0f03faf3b261a2f27bd14f75caef1413eafbe
omder1 <omar10riche@gmail.com>
2018-07-03 11:02:42 +0100

    installation de silex avec composer creations des htaccess et de l index sur la racine le projet est pret à decoler

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// toute tentative d’accès au script index.php se soldera par
+// une redirection vers le script index.php du répertoire web
+header('Location: web/index.php');
